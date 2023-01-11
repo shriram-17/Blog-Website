0a10f29b34114fb74fd5a0c55e1a7b0a14d1580b
shriram-17 <76253109+shriram-17@users.noreply.github.com>
2023-01-11 17:44:46 +0530

    11  Jan

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///blog.db'
+
+db =SQLAlchemy(app)
+
+class Blogpost(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle= db.Column(db.String(50))
+    author= db.Column(db.String(50))
+    date_posted= db.Column(db.DateTime)
+    content= db.Column(db.Text)
+    
+#db.create_all()
 
 @app.route('/')
 def index():
@@ -10,13 +26,30 @@ def index():
 def about():
     return render_template('about.html')  
 
-@app.route('/post')
-def post():
-    return render_template('post.html') 
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post=Blogpost.query.filter_by(id=post_id).one()
+    
+    return render_template('post.html',post=post) 
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route("/add")
+def add():
+    return render_template('add.html')
+
+@app.route("/addpost",methods=['POST'])
+def addpost():
+    title=request.form.get("title", False)
+    subtitle=request.form.get("subtitle", False)
+    author=request.form.get("author", False)
+    content=request.form.get("content", False)
+    post=Blogpost(title=title,subtitle=subtitle,author=author,content=content,date_posted=datetime.now())
+    db.session(post)
+    db.commit()
+    return '<h1>Title:{} Subtitle:{} Author: {} Content: {}</h1>'.format(title, subtitle,author,content)
      
 if __name__ =='__main__':
     app.run(debug=True)
